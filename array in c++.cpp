4fb9954111a24b203cabe6f4f636abd58ea79a2a
bylapudidurgaprasad8-creator <bylapudidurgaprasad8@gmail.com>
2025-11-27 20:37:29 +0530

    Add files via upload

---
+++ array in c++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[2][3]={
+	(1,2,3),
+	(4,5,6)
+	};
+	cout<<arr[2][2];
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<3;j++)
+		cout<<[i][j]<<endl;
+		
+	}
+}
